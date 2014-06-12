26ecea89415f1254c74c6194ce4ebc85f5974b0a
Jeremy Holmes <jholmes@ITman2.(none)>
2014-06-12 17:05:13 -0500

    Almost to 3.6

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -33,4 +33,14 @@ describe "StaticPages" do
       expect(page).to have_title('About')
     end
   end
+  describe "Contact Page" do
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+    it "should have title 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title('Contact')
+    end
+  end
 end
