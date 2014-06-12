2aede0e5309097e4f1eaf2f18b3238db8296ca92
Jeremy Holmes <jholmes@ITman2.(none)>
2014-06-12 16:20:30 -0500

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,11 +7,20 @@ describe "StaticPages" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Home")
+    end
   end
+
     describe "Help Page" do
       it "should have the the content 'Help'" do
         visit '/static_pages/help'
         expect(page).to have_content('Help')
+      end
+      it "should have the title 'Help'" do
+        visit '/static_pages/help'
+        expect(page).to have_title("Help")
       end
     end
   describe "About Page" do
@@ -19,5 +28,9 @@ describe "StaticPages" do
       visit '/static_pages/about'
       expect(page).to have_content('About Us')
     end
+    it "should have the title 'About'" do
+      visit '/static_pages/about'
+      expect(page).to have_title('About')
+    end
   end
-  end
+end
